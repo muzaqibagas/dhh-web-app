766b3035e45c00cbb3b790cfdc6d275af725ecdb
muzaqibagas <bagasmuzaqi1614@gmail.com>
2025-07-23 10:14:38 +0700

    bagas edit

--- resources/views/kolokium/index.blade.php
+++ resources/views/kolokium/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <strong>NANA HADIIRR</strong>
+    <strong>bagass ganteng</strong>    
     <h1>Daftar Kolokium</h1>
 
     <a href="{{ url('kolokium/create') }}">Tambah Kolokium</a>
