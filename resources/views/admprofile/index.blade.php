652725245c7f4089f7daeec2cda07d4c67aed222
muzaqibagas <bagasmuzaqi1614@gmail.com>
2025-12-19 08:21:23 +0700

    bagas: admin profile

--- resources/views/admprofile/index.blade.php
+++ resources/views/admprofile/index.blade.php
@@ -225,7 +225,7 @@
       <button class="btn-edit-photo">        
         <a href="{{ route('user.edit', Auth::user()->id) }}" class="bi bi-pencil-square text-light text-decoration-none"> Edit Profile</a>
       </button>    
-      @@endauth
+      @endauth
     </div>
 
     <div class="card p-4 shadow-sm w-100 border-2" style="border:solid #1b2a6d">
